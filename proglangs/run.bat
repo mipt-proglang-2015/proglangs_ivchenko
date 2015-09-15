6a1094bf2fa77d8e2b272b22f037cd22a8d42708
VeLKerr <Anellieme-n-fin@mail.ru>
2015-09-15 12:59:28 +0400

    Fixed 1

--- proglangs/run.bat
+++ proglangs/run.bat
@@ -2,6 +2,5 @@
 cmake -G "Visual Studio 12 2013 Win64" CMakeLists.txt 
 :: Linking, building and creating .dll
 cmake --build . --config Release 
-:: move and rename library for using as Python module
-move Release\repeating_count.dll 
-rename repeating_count.dll repeating_count.pyd 
+:: move for using as a Python module
+move Release\repeating_count.pyd
